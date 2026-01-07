d54d8ea03c6bb9498feef417bdfdb81d951ba6f5
subhambiswas <subham.biswas@innofied.com>
2026-01-07 19:09:25 +0530

    small change

--- src/main/java/com/security/auth/authsecurity/config/SecurityConfig.java
+++ src/main/java/com/security/auth/authsecurity/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.security.auth.authsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -29,5 +30,8 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    public AuthenticationManager authenticationManager
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration){
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
